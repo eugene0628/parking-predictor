2041439443ce20307dd75c6263338fb94b8cffcd
eugene0628 <youjeanz@gmail.com>
2023-10-17 14:11:51 -0400

    Added a map and reformatted demo site.

--- front-end/src/routes/api/callML/+server.ts
+++ front-end/src/routes/api/callML/+server.ts
@@ -1,25 +1,23 @@
 import { json, error } from '@sveltejs/kit';
-import { SECRET_NODE_LINK } from '$env/static/private';
+import { SECRET_FLASK_LINK, SECRET_NODE_LINK } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-
     const endpoint = `${SECRET_NODE_LINK}/predict`
-    const { garage1, garage2, garage3 } = await request.json();
-    // console.log(garage1)
-    // console.log(garage2)
+    const { inputs } = await request.json()
+
     const response = await fetch(endpoint, {
         method: "POST",
         headers: {
-            "Content-type": "application/json",
+            "Content-type":"application/json",
         },
-        body: JSON.stringify({ garage1, garage2, garage3 })
-    });
+        body: JSON.stringify({ inputs })
+    })
     if (response.ok) {
         const data = await response.json();
         return json(data);
     } else {
-        throw new Error("Failed to fulfill POST request to callML");
+        throw new Error("Failed to fulfill POST request to express.js predict API.")
     }
 }
 
