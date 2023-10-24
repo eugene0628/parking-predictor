31f0355ecb796bcc0f7498ff8f3b096901f269b3
Antonio Lau <104313812+antoniooioo@users.noreply.github.com>
2023-10-24 11:28:00 -0400

    updated ml model

--- ml-model/train.py
+++ ml-model/train.py
@@ -1,54 +1,58 @@
+
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import pickle
 # from matplotlib import pyplot as plt
 
-path1 = 'Sign1_full_fitted.csv'
-data1 = pd.read_csv(path1, usecols=[0,1], header=0)
-data1['ts'] = pd.to_datetime(data1['ts'])
-data1['unique_id'] = 'garage1'
-data1 = data1.rename(columns={'ts': 'ds', 'y1': 'y'})
+def load_and_preprocess_data(filepath, unique_id, column_name):
+    data = pd.read_csv(filepath, usecols=[0, 1], header=0)
+    data['ts'] = pd.to_datetime(data['ts'])
+    data['unique_id'] = unique_id
+    data = data.rename(columns={'ts': 'ds', column_name: 'y'})
+    return data
 
-path2 = 'Sign12_full_fitted.csv'
-data2 = pd.read_csv(path2, usecols=[0,1], header=0)
-data2['ts'] = pd.to_datetime(data2['ts'])
-data2['unique_id'] = 'garage2'
-data2 = data2.rename(columns={'ts': 'ds', 'y12': 'y'})
+def create_sliding_window(df, past_points=3, future_points=2, target_col='y'):
+    n = len(df)
+    cols = [target_col + f"_{i}" for i in range(-past_points, future_points)]
+    sliding_window_df = pd.DataFrame(columns=cols)
+    
+    for i in range(past_points, n - future_points + 1):
+        window_data = df[target_col].iloc[i-past_points:i+future_points].values
+        sliding_window_df.loc[i] = window_data
 
-path3 = 'Sign14_full_fitted.csv'
-data3 = pd.read_csv(path3, usecols=[0,1], header=0)
-data2['ts'] = pd.to_datetime(data3['ts'])
-data3['unique_id'] = 'garage3'
-data3 = data3.rename(columns={'ts': 'ds', 'y14': 'y'})
+    return sliding_window_df
 
-df = pd.concat([data1, data2, data3])
-df['ds'] = pd.to_datetime(df['ds'])
-df = df.drop('ts', axis=1)
+def generate_features_labels(df, past_points=3, future_points=2):
+    feature_cols = [col for col in df.columns if int(col.split("_")[-1]) < 0]
+    label_cols = [col for col in df.columns if int(col.split("_")[-1]) >= 0]
+    
+    X = df[feature_cols]
+    y = df[label_cols]
+    
+    return X, y
 
-train = df.loc[df['ds'] < '2016-11-17']
-valid = df.loc[(df['ds'] >= '2016-11-17')]
+def train_predictive_model(X, y):
+    model = LinearRegression()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Mean Squared Error: {mse}")
+    return model
 
-h = valid['ds'].nunique()
+def main_pipeline(filepath, unique_id, column_name, past_points=3, future_points=2):
+    data = load_and_preprocess_data(filepath, unique_id, column_name)
+    sliding_window_df = create_sliding_window(data, past_points, future_points, "y")
+    X, y = generate_features_labels(sliding_window_df, past_points, future_points)
+    model = train_predictive_model(X, y)
+    model_filename = f"{unique_id}_model.pkl"
+    with open(model_filename, "wb") as f:
+        pickle.dump(model, f)
+    print(f"Trained model saved as {model_filename}")
 
-from lightgbm import LGBMRegressor
-from mlforecast import MLForecast
-
-from window_ops.rolling import rolling_mean, rolling_max, rolling_min
-
-models = [LGBMRegressor(random_state=0, n_estimators=100)]
-
-model = MLForecast(models=models,
-                   freq='T',
-                   lags=[1440],
-                   date_features=['dayofweek'],
-                   )
-
-model.fit(train, id_col='unique_id', time_col='ds', target_col='y', static_features=[])
-
-pickle.dump(model, open('model.pkl', 'wb'))
-
-p = model.predict(horizon=h)
-
-last_values = p.groupby('unique_id').last()
-
-print(last_values)
+main_pipeline("Sign1_full_fitted.csv", "garage1", "y1")
+main_pipeline("Sign12_full_fitted.csv", "garage2", "y12")
+main_pipeline("Sign14_full_fitted.csv", "garage3", "y14")
