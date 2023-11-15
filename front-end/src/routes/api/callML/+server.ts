489f00cf8a18a8130419ef9b7d82f9874f669206
eugene0628 <youjeanz@gmail.com>
2023-11-14 20:42:48 -0500

    Added env

--- front-end/src/routes/api/callML/+server.ts
+++ front-end/src/routes/api/callML/+server.ts
@@ -6,34 +6,17 @@ export async function POST({ request }) {
     const endpoint = `${SECRET_NODE_LINK}/predict`
     const dataPackage = await request.json()
     console.log(JSON.stringify(dataPackage))
-    // const response = await fetch(endpoint, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-type":"application/json",
-    //     },
-    //     body: JSON.stringify( dataPackage )
-    // })
-
-    const randNums = {garage1: {expected_occupancy: 0}, garage2: {expected_occupancy: 0}, garage3: {expected_occupancy: 0}}
-
-    for (const num in randNums) {
-        randNums[num].expected_occupancy = Math.floor(Math.random() * 400);
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-type":"application/json",
+        },
+        body: JSON.stringify( dataPackage )
+    })
+    if (response.ok) {
+        const data = await response.json();
+        return json(data);
+    } else {
+        throw new Error("Failed to fulfill POST request to express.js predict API from callML Svelte API.")
     }
-
-    return json(randNums);
-
-    // const response = await fetch(endpoint, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-type":"application/json",
-    //     },
-    //     body: JSON.stringify({ inputs })
-    // })
-    // if (response.ok) {
-    //     const data = await response.json();
-    //     return json(data);
-    // } else {
-    //     throw new Error("Failed to fulfill POST request to express.js predict API.")
-    // }
 }
-
